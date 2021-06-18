8141c14b133082318ac7e90de8ee68b1a4a29002
Arnaldo Viera <arnaldoviera@gmail.com>
2021-06-18 20:56:24 -0300

    Commit para unificar el archivo de Jorgelina. No es mi commit

--- Trabajo Final Regresion Avanzada.R
+++ Trabajo Final Regresion Avanzada.R
@@ -21,9 +21,9 @@ library(funModeling)
 #Cargamos el dataset
 #dos aca cada uno pone si ingreso de la data
 #Cadauno usa su directorio
-setwd("C:/Users/vieraa/Documents/GitHub/RA_final" )   #establezco la carpeta donde voy a trabajar
+#setwd("C:/Users/vieraa/Documents/GitHub/RA_final" )   #establezco la carpeta donde voy a trabajar
 #setwd("C:/Users/quintej/Desktop/MCD/Regresion Avanzada/" )   #establezco la carpeta donde voy a trabajar
-#setwd("C:/Users/jgiberti/Documents/Univ. Austral - Maestria en Ciencias de Datos/10. Regresion Avanzada/TP/" )   #establezco la carpeta donde voy a trabajar
+setwd("C:/Users/jgiberti/Documents/Univ. Austral - Maestria en Ciencias de Datos/10. Regresion Avanzada/TP/" )   
 #setwd("C:/Users/isant/OneDrive/Desktop/MCD/Regresión Avanzada/TP Final")
 
 data <- fread("clinton.txt")
@@ -32,6 +32,7 @@ data <- fread("clinton.txt")
 # IRU -> A mi me parece que, si Brooklyn está siendo una observación influyente (NO outlier), quizás tenga sentido.
 # De todas maneras, habría que ver cómo performan ambos modelos y quedarnos con el mejor. 
 # Podemos plantear ambos. 
+
 data1<- dplyr::select(data, pje:crimen)
 
 
@@ -57,11 +58,13 @@ grafico2<-ggcorr(data1, nbreaks = 4, palette = "RdGy", label = TRUE, label_size 
 grafico2
 
 
+
 #JOR -> Grafico 1 De esta matriz de correlación puede observarse que las correlaciones mas altas se deben a pobreza e ingpc (0.617) pobreza y pje (0.501), veteranos y edad (0.526), ancianos y edad (0.48), crimen y densidad (0.405)
 #correlacion inversa entre ingpc y pobreza
 
 # IRU -> Yo lo comentaría pero en principio no me parecen demasiado preocupantes, 
 # no son tan altas, en todo caso, cuando analicemos modelo, vemos con cuál nos quedamos.
+
 
 
 library(psych)
@@ -77,10 +80,32 @@ multi.hist(x = data1, dcol = c("blue", "red"), dlty = c("dotted", "solid"),
 # IRU -> Podemos ver también de usar Ridge o Lasso?
 
 
+
+pairs(data1)
+#Analisis de correlacion
+cor(data1)
+
+# Mostramos visualmente la relaci?n de la variable dependiente con cada una de las variables independientes
+par(mfrow = c(2, 2))
+
+plot(data1$pje,data1$edad)
+plot(data1$pje,data1$ahorros)
+plot(data1$pje,data1$ingpc)
+plot(data1$pje,data1$pobreza)
+plot(data1$pje,data1$veteranos)
+plot(data1$pje,data1$mujeres)
+plot(data1$pje,data1$densidad)
+plot(data1$pje,data1$ancianos)
+plot(data1$pje,data1$crimen)
+
+par(mfrow = c(1, 1))
+
+
 ### Jor --> 2. GENERACION DEL MODELO###
 
 #creo modelo de regresion multiple sin modificaciones
 mod1<-lm(pje ~ edad+ahorros+ingpc+pobreza+veteranos+mujeres+densidad+ancianos+crimen, data=data )
+
 summary(mod1)
 
 #JOR -> Coef determinacion muy chico, casi no hay relacion de las variables explicat con la variable respuesta
@@ -92,11 +117,30 @@ summary(mod1)
 
 plot(mod1)
 
-
 # el dato 1602 esta completamente desubicado
 
 # IRU -> El gráfico QQ da distribución con outliers. OJO con el 1602 porque quizás es influyente y no outlier
 # y puede requerir otro tratamiento. 
+
+
+#Jor -->Analisis de residuos
+
+
+rstandard(mod1)
+rstudent(mod1)
+hatvalues(mod1)
+library(qpcR)
+PRESS(mod1)$residuals
+
+plot(x,y)
+barplot(PRESS(mod1)$residuals)
+plot(PRESS(mod1)$residuals, type="l")
+
+
+#Jor --> Supuesto de normalidad
+
+library(UsingR)
+residuos <- residuals(mod1)
 
 #JOR ->
 shapiro.test(mod1$residuals)
@@ -104,48 +148,66 @@ shapiro.test(mod1$residuals)
 # el test de hipótesis no confirma la normalidad.
 #Es necesario SI O SI Aplicar alguna transformacion a las variables y despues vemos el resto
 
-## Arnaldo => esto que sigue sirve para algo?
-criterios <- function(mmod1, maxi) {
-  
-  #analisis de residuos
-  PRESS(mod1)$residuals
-  
-  
-  SCE <- deviance(mod1)
-  n <- length(residuals(mod1))
-  p <- length(coefficients(mod1)) #incluye intercepto
-  
-  #CME
-  cme <- SCE/(n-p) #igual al cuadrado de la residual SE
-  
-  #R2 ajustado
-  r2aj <- 1 - ((n-1)/(n-p)) * (1-summary(mod1)$r.squared)
-  
-  summary(mod1)$adj.r.squared
-  yi <- iris$Petal.Length
-  1 - (deviance(mod1) / (n-p)) / ((sum((yi - mean(yi))^2))/(n-1))
-  
-  #PRESS
-  estpress <- sum((residuals(mod1)/(1-hatvalues(mod1)))^2) #qpcR::PRESS(m)
-  
-  #Mallows CP
-  mcp <- SCE/(deviance(maxi)/(n-length(coefficients(maxi)))) + 2*p - n
-  #olsrr::ols_mallows_cp(m, maxi)
-  
-  #AKAIKE
-  akaike <- n * log(SCE/n) + 2*p #extractAIC(m)[2]
-  
-  #BIC
-  schwarz <- n * log(SCE/n) + p*log(n) #extractAIC(m, k = log(n))[2]
-  
-  tibble(CME = cme, R2Aj = r2aj, PRESS = estpress, Cp = mcp, AIC = akaike, BIC = schwarz)
-  
-}
 
-#mÃ©todo forward
+
+#Jor --> Transf BoxCox
+mod1_boxcox <- boxcox(pje ~ edad+ahorros+ingpc+pobreza+veteranos+mujeres+densidad+ancianos+crimen, data=data)
+
+mod1$edad[which.max(mod1$pje)] 
+
+mod1$ahorros[which.max(mod1$pje)] 
+
+mod1$ingpc[which.max(mod1$pje)] 
+
+mod1$pobreza[which.max(mod1$pje)] 
+
+mod1$veteranos[which.max(mod1$pje)] 
+
+mod1$mujeres[which.max(mod1$pje)] 
+
+mod1$densidad[which.max(mod1$pje)] 
+
+mod1$ancianos[which.max(mod1$pje)] 
+
+mod1$crimen[which.max(mod1$pje)] 
+
+#Todos me dieron 0..Lo que indicaria que habria que transformar la variable
+
+data$logpje <- log(data$pje) * exp(mean(log(data$pje))) #log X media geom
+
+
+#Jor -- Distancia de Cook
+
+cooks.distance(mod1)
+#Jor --> Todos los puntos son mayores a 1, entonces son puntos influyentes??
+
+influence.measures(mod1)
+# Jor -> hay que analizar la tabla que tira
+
+
+
+# Jor _> Seleccion del modelo
+#Colinealidad entre las variables. ¿Existe? ¿Tenemos que hacer algo?
+# por lo que se visuaaliza en la matriz de corr la correlacion es baja..
+#no hay indicio de colinealidad
+
+
+#Jor --> Verifico con VIF
+vif(lm(pje ~ edad+ahorros+ingpc+pobreza+veteranos+mujeres+densidad+ancianos+crimen, data=data ))
+
+
+#edad      ahorros     ingpc   pobreza   veteranos   mujeres   densidad  ancianos   crimen
+#1.733274  1.610381  2.428866  2.057863  1.551142    1.177141  1.304469  1.611870    1.527998
+
+#Jor --> no hay VIF mayores a 5, o sea no habria problema con la colinealidad
+
+
+
+
+#Jor --> metodo forward
 
 library(MASS)
-data(data)
+data(data1)
 
 stepAIC(
   object = lm(pje ~ 1, data = data), #punto de partida
@@ -173,28 +235,30 @@ mod_seqrep <- regsubsets (pje ~ edad + ahorros + ingpc + pobreza + veteranos + m
 
 ######################################################
 
+#Jor --> Regresion Ridge
+library(MASS)
+lm.ridge(
+  formula = pje ~ edad + ahorros + ingpc + pobreza + veteranos + mujeres + densidad + ancianos + crimen, 
+  data = data1, 
+  lambda = seq(0, 1, by = 0.1)
+)
 
-#Jor -> stepwise mixto. 
+#############################################################
 
 
-#El valor matemático empleado para determinar la calidad del modelo va a ser Akaike(AIC).
+######################################################
 
-step(object = mod1, direction = "both", trace = 1)
+#Jor --> Regresion Lasso
+library(glmnet)
+#Armar
 
-mod2<-lm(formula = pje ~ ahorros + ingpc + pobreza + veteranos + mujeres + 
-           densidad + ancianos + crimen, data = data)
+#############################################################
 
-summary(mod2)
-##DA HORRIBLE EL MODELO, POR ENDE HAY QUE VER PREVIAMENTE QUE HACEMOS CON ESA OBSERVACION
 
-## Arnaldo = opinino que debemos quitarla
-
-########################################################
-
-####lleguÃ© hasta acÃ¡!!!!!!!!
 
 #Criterios de selecciÃ³n de modelos
 resumen$cp
+
 resumen$adjr2
 resumen$bic
 
